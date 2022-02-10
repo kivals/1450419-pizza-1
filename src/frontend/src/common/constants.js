2e21786dd14c2aa035fa4616a02c4283f92ae67e
melnik <melnik@bifit.com>
2022-02-10 17:00:46 +0300

    Solution for module1-task1

---
+++ src/frontend/src/common/constants.js
@@ -0,0 +1,99 @@
+export const doughTypes = [
+  {
+    name: "Тонкое",
+    value: "light",
+  },
+  {
+    name: "Толстое",
+    value: "large",
+  },
+];
+
+export const sauceTypes = [
+  {
+    name: "Томатный",
+    value: "tomato",
+  },
+  {
+    name: "Сливочный",
+    value: "creamy",
+  },
+];
+
+export const ingredientTypes = [
+  {
+    name: "Грибы",
+    value: "mushrooms",
+  },
+  {
+    name: "Чеддер",
+    value: "cheddar",
+  },
+  {
+    name: "Салями",
+    value: "salami",
+  },
+  {
+    name: "Ветчина",
+    value: "ham",
+  },
+  {
+    name: "Ананас",
+    value: "ananas",
+  },
+  {
+    name: "Бекон",
+    value: "bacon",
+  },
+  {
+    name: "Лук",
+    value: "onion",
+  },
+  {
+    name: "Чили",
+    value: "chile",
+  },
+  {
+    name: "Халапеньо",
+    value: "jalapeno",
+  },
+  {
+    name: "Маслины",
+    value: "olives",
+  },
+  {
+    name: "Томаты",
+    value: "tomatoes",
+  },
+  {
+    name: "Лосось",
+    value: "creamy",
+  },
+  {
+    name: "Моцарелла",
+    value: "mozzarella",
+  },
+  {
+    name: "Пармезан",
+    value: "parmesan",
+  },
+  {
+    name: "Блю чиз",
+    value: "blue_cheese",
+  },
+];
+
+export const sizeTypes = [
+  {
+    name: "23 см",
+    value: "small",
+  },
+  {
+    name: "32 см",
+    value: "normal",
+  },
+  {
+    name: "45 см",
+    value: "big",
+  },
+];
