f76f77fa9ea4c1cf01290635e42829151465f542
melnik <melnik@bifit.com>
2022-05-20 13:57:33 +0300

    Fixed moving from success page

--- src/frontend/src/views/SuccessPopup.vue
+++ src/frontend/src/views/SuccessPopup.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="popup" @click.prevent="close()">
-    <a href="#" class="close">
+  <div class="popup">
+    <router-link class="close" :to="goToUrl">
       <span class="visually-hidden">Закрыть попап</span>
-    </a>
+    </router-link>
     <div class="popup__title">
       <h2 class="title">Спасибо за заказ</h2>
     </div>
     <p>Мы начали готовить Ваш заказ, скоро привезём его вам ;)</p>
     <div class="popup__button">
-      <a href="#" class="button" @click.prevent="close()">Отлично, я жду!</a>
+      <router-link class="button" :to="goToUrl">Отлично, я жду!</router-link>
     </div>
   </div>
 </template>
@@ -20,12 +20,8 @@ export default {
   name: "SuccessPopup",
   computed: {
     ...mapState("Auth", ["isAuthenticated"]),
-  },
-  methods: {
-    close() {
-      this.isAuthenticated
-        ? this.$router.push("/orders")
-        : this.$router.push("/");
+    goToUrl() {
+      return this.isAuthenticated ? "orders" : "/";
     },
   },
 };
